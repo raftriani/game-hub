d42d9b7de8aa1ae3d5c3a389e219214fa7a2bad3
rafael.triani <rafael.triani@ergondata.com.br>
2023-08-02 15:26:40 -0300

    Build a navbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
-import "./index.css";
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider></ChakraProvider>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
