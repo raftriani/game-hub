f8c9752f774cad3a2bc0d473def01bc78bba9def
rafael.triani <rafael.triani@ergondata.com.br>
2023-08-03 11:44:51 -0300

    Remove duplicates styles

--- src/components/GameGardSkeleton.tsx
+++ src/components/GameGardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameGardSkeleton = () => {
   return (
-    <Card width="300px">
+    <Card>
       <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
