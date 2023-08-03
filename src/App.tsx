683334323574ff6cb29d308886b10f1a0e1a31f1
rafael.triani <rafael.triani@ergondata.com.br>
2023-08-03 14:40:11 -0300

    Buid platform select

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelect from "./components/PlatformSelect";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -31,6 +32,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
+        <PlatformSelect />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
